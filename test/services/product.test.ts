3bf22d390f32c980b081dc3b804ec438d9e3f174
Prasanna_emmadi <prasanna.177@gmail.com>
2023-12-01 14:05:34 +0200

    fixing product test

--- test/services/product.test.ts
+++ test/services/product.test.ts
@@ -1,101 +1,90 @@
 import ProductService from "../../services/productsService";
 import connect, { MongoHelper } from "../db-helper";
 import ProductRepo from "../../models/Product";
-import CategoryModel from "../../models/Category";
+import categoryService from "../../services/categoryService";
 
 describe("Product service", () => {
   let mongoHelper: MongoHelper;
+  // product is dependent on
+  // category
+  // create category
+  let categoryId: string;
 
   beforeAll(async () => {
     mongoHelper = await connect();
+    // create category
+    const categoryObj: any = {
+      _id: "655e1356be9cf967bdead01f",
+      name: "test",
+    };
+    const category: any = await categoryService.createOne(categoryObj);
+    categoryId = category._id;
   });
-
-  beforeEach(async () => {
-    await CategoryModel.create({ name: "Test Category" }); // Create test category
-  });
-
-  afterEach(async () => {
-    await mongoHelper.clearDatabase();
-  });
-
   afterAll(async () => {
     await mongoHelper.closeDatabase();
   });
-
-  it("should create a new product", async () => {
-    // create new product
+  // create new product
+  async function createProduct() {
     const product: any = {
-      _id: "655e1356be9cf967bdead01f",
       name: "Test Product",
       description: "This is a test product.",
       price: 19.99,
       image: "test-image-url.jpg",
-      categoryId: "655e1356be9cf967bdead01f",
     };
-
-    const newProduct = await ProductService.createOne(
-      product,
-      "655e1356be9cf967bdead01f"
-    );
-
-    expect(newProduct).toHaveProperty("name");
+    const newProduct = await ProductService.createOne(product, categoryId);
+    expect(newProduct).toHaveProperty("_id");
     expect(newProduct.name).toEqual("Test Product");
+    expect(newProduct.description).toEqual("This is a test product.");
+    expect(newProduct.price).toEqual(19.99);
+    expect(newProduct.image).toEqual("test-image-url.jpg");
+    return newProduct;
+  }
+
+  it("should create a new product", async () => {
+    await createProduct();
   });
 
-  it("should return a list of products", async () => {
-    // create new product
-    const newProduct = new ProductRepo({
+  it("should get one product", async () => {
+    const product = await createProduct();
+    const id = product._id.toString();
+    const fetchedProduct = await ProductService.findOne(id);
+    if (fetchedProduct) {
+      const newId = fetchedProduct._id.toString();
+      expect(newId).toBe(id);
+    }
+  });
+  it("should update a new product", async () => {
+    const product = await createProduct();
+    const updatedproduct: any = {
       name: "Test Product",
       description: "This is a test product.",
       price: 19.99,
-      image: "https://example.com/smartphone.jpg",
-      categoryId: "655e9e08106158f8d895ccbe",
-    });
-
-    await newProduct.save();
-
-    const products = await ProductService.findAll();
-
-    expect(products.length).toEqual(1);
-    expect(products[0]).toHaveProperty("name", "Test Product");
-  });
-
-  it("should find a product by ID", async () => {
-    // create new product
-    const newProduct = new ProductRepo({
-      name: "Test Product",
-      description: "This is a test product.",
-      price: 19.99,
-      image: "https://example.com/smartphone.jpg",
-      categoryId: "655e9e08106158f8d895ccbe",
-    });
-
-    const savedProduct = await newProduct.save();
-
-    const foundProduct = await ProductService.findOne(savedProduct._id);
-
-    expect(foundProduct).toHaveProperty("name", "Test Product");
-  });
-
-  it("should delete a product by ID", async () => {
-    // create new product
-    const newProduct = new ProductRepo({
-      name: "Test Product",
-      description: "This is a test product.",
-      price: 19.99,
-      image: "https://example.com/smartphone.jpg",
-      categoryId: "655e9e08106158f8d895ccbe",
-    });
-
-    const savedProduct = await newProduct.save();
-
-    const deletedProduct = await ProductService.deleteOne(savedProduct._id);
-
-    expect(deletedProduct).toHaveProperty("name", "Test Product");
-    expect(deletedProduct).toHaveProperty(
-      "description",
-      "This is a test product."
+      image: "test-image-url.jpg",
+    };
+    const id = product._id.toString();
+    const newProduct = await ProductService.updateOne(
+      id,
+      updatedproduct,
+      categoryId
     );
-    expect(deletedProduct).toHaveProperty("price", 19.99);
+    if (newProduct) {
+      expect(newProduct).toHaveProperty("_id");
+      expect(newProduct.name).toEqual("Test Product");
+      expect(newProduct.description).toEqual("This is a test product.");
+      expect(newProduct.price).toEqual(19.99);
+      expect(newProduct.image).toEqual("test-image-url.jpg");
+    }
+  });
+  it("should delete a product", async () => {
+    const product = await createProduct();
+    const id = product._id.toString();
+    const newProduct = await ProductService.deleteOne(id);
+    if (newProduct) {
+      expect(newProduct).toHaveProperty("_id");
+      expect(newProduct.name).toEqual("Test Product");
+      expect(newProduct.description).toEqual("This is a test product.");
+      expect(newProduct.price).toEqual(19.99);
+      expect(newProduct.image).toEqual("test-image-url.jpg");
+    }
   });
 });
