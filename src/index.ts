5c03de9ae5d8e085043408bc12fdf505ce733838
Prasanna_emmadi <prasanna.177@gmail.com>
2023-12-11 15:15:33 +0200

    deleteing commented code

--- src/index.ts
+++ src/index.ts
@@ -42,32 +42,6 @@ app.get("/api/v1/protected", checkAuth, (req, res) => {
   res.json({ items: [1, 2, 3, 4, 5] });
 });
 
-// for generating secret key
-// const key = crypto.randomBytes(64).toString("hex")
-// console.log("Key:",key)
-//login code shifted to user controller
-// app.post("/api/v1/login", (req, res) =>{
-//   const user = {
-//     id: 'abcde-fghijk',
-//     email :'test@test.io',
-//     password :'12345',
-//   }
-
-//   if(req.body.email !== user.email || req.body.password !== user.password){
-//     return res.json({
-//       msg:'Invalid credential'
-//     })
-//   }
-//   const payload = {
-//     userId: user.id,
-//     email :user.email,
-//   }
-//  const token = jwt.sign(payload,"secret")
-//   //const token = jwt.sign(payload, process.env.TOKEN_SECRET as string)
-//   console.log("Token:",token)
-//   res.json({token})
-// });
-
 app.use(responseHandler);
 app.use(routeNotFound);
 
