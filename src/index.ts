8a9d3b925f25d8a24c70cc28ec6804b7e03d1770
Prasanna_emmadi <prasanna.177@gmail.com>
2023-12-13 16:39:40 +0200

    Adding swagger api documentation link

--- src/index.ts
+++ src/index.ts
@@ -14,42 +14,44 @@ import { responseHandler } from "./middlewares/responsehandler";
 import orderDetailsRoute from "./routes/orderItemsRoute";
 import authRoute from "./routes/authRoute";
 
-const PORT = 8080;
+const PORT = 8081;
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-// TODO: Validate .env using Zod
-if (process.env.NODE_ENV === "DEV" || process.env.NODE_ENV === "PRODUCTION") {
-  const mongoURL = process.env.DB_URL as string;
-  mongoose.connect(mongoURL).then(() => console.log("Connected!"));
-}
+const initServer = async () => {
+  app.use(express.json());
+  app.use(cors());
+  // TODO: Validate .env using Zod
+  if (process.env.NODE_ENV === "DEV" || process.env.NODE_ENV === "PRODUCTION") {
+    const mongoURL = process.env.DB_URL as string;
+    mongoose.connect(mongoURL).then(() => console.log("Connected!"));
+  }
 
-app.use("/docs", async () => {
-  return await TspecDocsMiddleware();
-});
+  app.use("/api/v1/docs", await TspecDocsMiddleware());
 
-app.get("/hello", loggingMiddleware, (_, res) => {
-  res.json({ msg: "hello, from Express.js!" });
-});
-
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/products", productsRoute);
-app.use("/api/v1/categories", categoryRoute);
-app.use("/api/v1/users", usersRoute);
-app.use("/api/v1/orders", orderRoute);
-app.use("/api/v1/orderItems", orderDetailsRoute);
-
-app.get("/api/v1/protected", checkAuth, (req, res) => {
-  res.json({ items: [1, 2, 3, 4, 5] });
-});
-
-app.use(responseHandler);
-app.use(routeNotFound);
-
-if (process.env.NODE_ENV === "DEV" || process.env.NODE_ENV === "PRODUCTION") {
-  app.listen(PORT, () => {
-    console.log(`👀 app is running at localhost:${PORT}`);
+  app.get("/hello", loggingMiddleware, (_, res) => {
+    res.json({ msg: "hello, from Express.js!" });
   });
-}
+
+  app.use("/api/v1/auth", authRoute);
+  app.use("/api/v1/products", productsRoute);
+  app.use("/api/v1/categories", categoryRoute);
+  app.use("/api/v1/users", usersRoute);
+  app.use("/api/v1/orders", orderRoute);
+  app.use("/api/v1/orderItems", orderDetailsRoute);
+
+  app.get("/api/v1/protected", checkAuth, (req, res) => {
+    res.json({ items: [1, 2, 3, 4, 5] });
+  });
+
+  app.use(responseHandler);
+  app.use(routeNotFound);
+
+  if (process.env.NODE_ENV === "DEV" || process.env.NODE_ENV === "PRODUCTION") {
+    app.listen(PORT, () => {
+      console.log(`👀 app is running at localhost:${PORT}`);
+    });
+  }
+};
+
+initServer().catch((e) => console.error(e));
 export default app;
